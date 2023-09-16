8a004d80e8213483dbc9ed848559b3e7cd29994a
Manik Dingra <95385759+M-A-N-I-K@users.noreply.github.com>
2023-09-17 01:24:30 +0530

    Sidebar removed

--- src/Components/InputComponent.jsx
+++ src/Components/InputComponent.jsx
@@ -50,7 +50,7 @@ const InputComponent = () => {
     }, [result])
 
     return (
-        <div className='w-[85vw] h-[100vh] py-4 overflow-y-scroll flex flex-col justify-between items-center'>
+        <div className='w-[100vw] h-[100vh] py-4 overflow-y-scroll flex flex-col justify-between items-center'>
             <div className='w-3/4 h-[40vh]'>
                 <div className="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
                     <div className="flex items-center justify-center px-3 py-2 border-b dark:border-gray-600">
@@ -59,7 +59,7 @@ const InputComponent = () => {
                             <span className="sr-only">Microphone</span>
                         </button>
                         <button type="button" className="p-2 text-gray-500 rounded cursor-pointer hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600">
-                            <svg className="w-6 h-6" fill="currentColor" height="200px" width="200px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 487 487" xmlSpace="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <g> <path d="M308.1,277.95c0,35.7-28.9,64.6-64.6,64.6s-64.6-28.9-64.6-64.6s28.9-64.6,64.6-64.6S308.1,242.25,308.1,277.95z M440.3,116.05c25.8,0,46.7,20.9,46.7,46.7v122.4v103.8c0,27.5-22.3,49.8-49.8,49.8H49.8c-27.5,0-49.8-22.3-49.8-49.8v-103.9 v-122.3l0,0c0-25.8,20.9-46.7,46.7-46.7h93.4l4.4-18.6c6.7-28.8,32.4-49.2,62-49.2h74.1c29.6,0,55.3,20.4,62,49.2l4.3,18.6H440.3z M97.4,183.45c0-12.9-10.5-23.4-23.4-23.4c-13,0-23.5,10.5-23.5,23.4s10.5,23.4,23.4,23.4C86.9,206.95,97.4,196.45,97.4,183.45z M358.7,277.95c0-63.6-51.6-115.2-115.2-115.2s-115.2,51.6-115.2,115.2s51.6,115.2,115.2,115.2S358.7,341.55,358.7,277.95z"></path> </g> </g> </g></svg>
+                            <svg className="w-6 h-6" fill="currentColor" height="200px" width="200px" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" viewBox="0 0 487 487" xmlSpace="preserve"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRep_iconCarrier"> <g> <g> <path d="M308.1,277.95c0,35.7-28.9,64.6-64.6,64.6s-64.6-28.9-64.6-64.6s28.9-64.6,64.6-64.6S308.1,242.25,308.1,277.95z M440.3,116.05c25.8,0,46.7,20.9,46.7,46.7v122.4v103.8c0,27.5-22.3,49.8-49.8,49.8H49.8c-27.5,0-49.8-22.3-49.8-49.8v-103.9 v-122.3l0,0c0-25.8,20.9-46.7,46.7-46.7h93.4l4.4-18.6c6.7-28.8,32.4-49.2,62-49.2h74.1c29.6,0,55.3,20.4,62,49.2l4.3,18.6H440.3z M97.4,183.45c0-12.9-10.5-23.4-23.4-23.4c-13,0-23.5,10.5-23.5,23.4s10.5,23.4,23.4,23.4C86.9,206.95,97.4,196.45,97.4,183.45z M358.7,277.95c0-63.6-51.6-115.2-115.2-115.2s-115.2,51.6-115.2,115.2s51.6,115.2,115.2,115.2S358.7,341.55,358.7,277.95z"></path> </g> </g> </g></svg>
                         </button>
                     </div>
                     <div className="px-4 py-2 bg-white rounded-b-lg dark:bg-gray-800">
@@ -70,10 +70,11 @@ const InputComponent = () => {
                 <button type="button" onClick={getQuery} className="inline-flex  w-full justify-center items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg focus:ring-4 focus:ring-blue-200 dark:focus:ring-blue-900 hover:bg-blue-800">
                     Generate Solution
                 </button>
-                <div className="relative flex justify-center py-4 overflow-hidden rounded-lg shadow-lg cursor-pointer">
-                    <img className="object-cover rounded-lg w-72" src="https://images.unsplash.com/photo-1502581827181-9cf3c3ee0106?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=420&q=80" alt="Flower and sky" />
-
-                    <div className="absolute top-2 left-60 px-6 py-4">
+            </div>
+            {
+                moodCard &&
+                <div className="flex justify-center py-4 overflow-hidden rounded-lg shadow-lg cursor-pointer">
+                    <div className="flex flex-col justify-start bg-white px-6 py-4">
                         <h4 className="mb-3 text-start text-xl font-semibold tracking-tight text-white">Mood Card</h4>
 
                         {
@@ -85,7 +86,7 @@ const InputComponent = () => {
                         }
                     </div>
                 </div>
-            </div>
+            }
         </div>
 
     )
